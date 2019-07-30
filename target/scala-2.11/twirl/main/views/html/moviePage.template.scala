06936a92d89ec7d4549550987f4849eb4362a89b
ktennyson <kilian.tennyson@adaptivemobile.com>
2019-07-30 13:27:00 +0100

    selection a colour changes the background for Movie Review page

--- target/scala-2.11/twirl/main/views/html/moviePage.template.scala
+++ target/scala-2.11/twirl/main/views/html/moviePage.template.scala
@@ -38,15 +38,15 @@ Seq[Any](format.raw/*2.1*/("""<script src=""""),_display_(/*2.15*/routes/*2.21*/
     """),_display_(/*7.6*/for(m <- movies) yield /*7.22*/{_display_(Seq[Any](format.raw/*7.23*/("""
 
         
-
+        
         """),_display_(/*11.10*/m/*11.11*/.getBackgroundColour),format.raw/*11.31*/("""
 
 
-        """),format.raw/*14.9*/("""<img onload="changeBackground("""),_display_(/*14.40*/m/*14.41*/.getBackgroundColour),format.raw/*14.61*/(""")" src="/assets/"""),_display_(/*14.78*/m/*14.79*/.getMoviePoster),format.raw/*14.94*/("""">
+        """),format.raw/*14.9*/("""<img onload="changeBackground('"""),_display_(/*14.41*/m/*14.42*/.getBackgroundColour),format.raw/*14.62*/("""')" src="/assets/"""),_display_(/*14.80*/m/*14.81*/.getMoviePoster),format.raw/*14.96*/("""">
         /assets/"""),_display_(/*15.18*/m/*15.19*/.getMoviePoster),format.raw/*15.34*/("""
         """),_display_(/*16.10*/for(colour <- m.generateColours(10)) yield /*16.46*/{_display_(Seq[Any](format.raw/*16.47*/("""
            
-            """),format.raw/*18.13*/("""<div style="background-color:"""),_display_(/*18.43*/colour),format.raw/*18.49*/("""; width:15em;">
+            """),format.raw/*18.13*/("""<div id="div" onClick="changeBackground()"  style="background-color:"""),_display_(/*18.82*/colour),format.raw/*18.88*/("""; width:15em;">
                     <a href=""""),_display_(/*19.31*/routes/*19.37*/.HomeController.changeMovieColour(m.getId, colour)),format.raw/*19.87*/(""""> """),_display_(/*19.91*/colour),format.raw/*19.97*/("""</a>
             </div>
 
@@ -71,10 +71,10 @@ Seq[Any](format.raw/*2.1*/("""<script src=""""),_display_(/*2.15*/routes/*2.21*/
 
               /*
                   -- GENERATED --
-                  DATE: Tue Jul 30 11:51:15 IST 2019
+                  DATE: Tue Jul 30 13:23:59 IST 2019
                   SOURCE: /home/ktennyson/Documents/pacticeFiles/whattowatch/app/views/moviePage.scala.html
-                  HASH: d30b7f7c345f0a274bccecdfe8a21a952e2a6e43
-                  MATRIX: 970->1|1098->36|1138->50|1152->56|1217->101|1279->138|1300->151|1339->153|1370->158|1427->190|1458->206|1496->207|1544->228|1554->229|1595->249|1633->260|1691->291|1701->292|1742->312|1786->329|1796->330|1832->345|1879->365|1889->366|1925->381|1962->391|2014->427|2053->428|2106->453|2163->483|2190->489|2263->535|2278->541|2349->591|2380->595|2407->601|2472->635|2508->641|2542->645
+                  HASH: 9cb30097da113b0c3380389d7a974d239cf93284
+                  MATRIX: 970->1|1098->36|1138->50|1152->56|1217->101|1279->138|1300->151|1339->153|1370->158|1427->190|1458->206|1496->207|1552->236|1562->237|1603->257|1641->268|1700->300|1710->301|1751->321|1796->339|1806->340|1842->355|1889->375|1899->376|1935->391|1972->401|2024->437|2063->438|2116->463|2212->532|2239->538|2312->584|2327->590|2398->640|2429->644|2456->650|2521->684|2557->690|2591->694
                   LINES: 28->1|33->2|33->2|33->2|33->2|35->4|35->4|35->4|36->5|38->7|38->7|38->7|42->11|42->11|42->11|45->14|45->14|45->14|45->14|45->14|45->14|45->14|46->15|46->15|46->15|47->16|47->16|47->16|49->18|49->18|49->18|50->19|50->19|50->19|50->19|50->19|53->22|54->23|57->26
                   -- GENERATED --
               */
--- target/web/classes/main/META-INF/resources/webjars/whattowatch/1.0-SNAPSHOT/javascripts/moviePage.js
+++ target/web/classes/main/META-INF/resources/webjars/whattowatch/1.0-SNAPSHOT/javascripts/moviePage.js
@@ -1,6 +1,6 @@
 
 function changeBackground(colour){
     console.log("sd");
-    document.getElementById("body").style.backgroundColor = "black";
 
+    document.getElementById("body").style.backgroundColor = colour;
 };
